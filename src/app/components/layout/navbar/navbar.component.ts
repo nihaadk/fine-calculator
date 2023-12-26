34321dc1a5b4ed4ed456710ba5c9081aef4319ad
nihaadk <nihad.keric@emineo.ch>
2023-12-26 23:39:58 +0100

    Update layout for responsive design

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ToggleThemeButtonComponent } from '../../toggle-theme-button/toggle-theme-button.component';
-import { LanguageDropdownComponent } from '../../language-dropdown/language-dropdown.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { LanguageDropdownComponent } from '../../language-dropdown/language-dropdown.component';
+import { ToggleThemeButtonComponent } from '../../toggle-theme-button/toggle-theme-button.component';
 
 @Component({
   selector: 'app-navbar',
@@ -16,7 +16,9 @@ import { TranslateModule } from '@ngx-translate/core';
   template: `
     <div class="navbar bg-base-100">
       <div class="flex-1">
-        <span class="text-2xl text-primary font-bold font-mono cursor-default" translate
+        <span
+          class="text-sm md:text-2xl text-primary font-bold font-mono cursor-default"
+          translate
           >APP_TITLE</span
         >
       </div>
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { EntryFormComponent } from '../../components/layout/entry-form/entry-form.component';
 import { ResultComponent } from '../../components/layout/result/result.component';
-import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, EntryFormComponent, ResultComponent, TranslateModule],
   template: `
-    <div class="flex w-full">
-      <div class="w-1/2">
+    <div class="flex flex-col sm:flex-row">
+      <div class="w-full sm:w-1/2">
         <div class="card bg-base-100 shadow-xl">
           <div class="card-body">
             <h2 class="card-title text-4xl justify-center divider divider-primary" translate>
@@ -21,8 +21,8 @@ import { TranslateModule } from '@ngx-translate/core';
           </div>
         </div>
       </div>
-      <div class="divider divider-horizontal"></div>
-      <div class="w-1/2">
+      <div class="divider divider-vertical sm:divider-horizontal"></div>
+      <div class="w-full sm:w-1/2">
         <app-result />
       </div>
     </div>
