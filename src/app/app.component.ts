fdba09ddc04e65ececee2b23f3dc0a3973916d9b
nihaadk <nihad.keric@emineo.ch>
2023-12-27 15:52:48 +0100

    Remove warning dialog component and update imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
-import { WarningDailogComponent } from './components/warning-dailog/warning-dailog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    NavbarComponent,
-    FooterComponent,
-    WarningDailogComponent,
-  ],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   template: `
-    <app-warning-dailog />
     <main>
       <app-navbar />
       <div class="mx-auto p-10 bg-primary-content">
