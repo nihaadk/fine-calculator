fe3a01959cae5151917c48a434dcde3a2d92d3ef
nihaadk <nihadkeric@gmail.com>
2025-09-22 19:48:37 +0200

    feat: update to 18

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -1,14 +1,15 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
 import { routes } from '../app.routes';
 import { TRANSLATE_CONFIG } from './translate.config';
-import { HttpClientModule } from '@angular/common/http';
-import { TranslateModule } from '@ngx-translate/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule, TranslateModule.forRoot(TRANSLATE_CONFIG)),
+    provideHttpClient(),
+    importProvidersFrom(TranslateModule.forRoot(TRANSLATE_CONFIG)),
   ],
 };
